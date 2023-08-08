f66b0f7ca93ffef4edfeadeb2d7d812618595d12
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-08 14:58:11 +0300

    add task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        n = ord(c)
+        print("{:c}".format(n - 32 if n >= 97 and n <= 122 else n), end="")
+    else:
+        print()
+
+uppercase("mostafa elmasry 98")
