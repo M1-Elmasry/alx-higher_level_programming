9d1036d085c1d9d4bd36805b95864a885754fe63
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-10 18:27:30 +0300

    update task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,19 @@
 from sys import argv
 
 
-def print_args():
-    n_args = len(argv)
-    if n_args > 1:
+def print_args(args):
+    n_args = len(args)
+    if n_args > 2:
         print("{:d} arguments:".format(n_args - 1))
         for n in range(1, n_args):
             print("{:d}: {:s}".format(n, argv[n]))
+
+    elif n_args == 2:
+         print("1 argument:")
+
     else:
         print("0 arguments.")
 
 
 if __name__ == "__main__":
-    print_args()
+    print_args(argv)
