0901303b5dd8f2b46046176deba52c8bdff61ab4
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-10 18:31:37 +0300

    update task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,10 @@ from sys import argv
 
 def print_args(args):
     n_args = len(args)
-    if n_args > 2:
-        print("{:d} arguments:".format(n_args - 1))
+    if n_args > 1:
+        print("{:d} argument{:s}".format(n_args - 1, ":" if n_args == 2 else "s:"))
         for n in range(1, n_args):
             print("{:d}: {:s}".format(n, argv[n]))
-
-    elif n_args == 2:
-         print("1 argument:")
-
     else:
         print("0 arguments.")
 
