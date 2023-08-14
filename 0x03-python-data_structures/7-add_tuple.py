a159702d03dd7eb4d20fa12148c5763b63057386
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-15 00:16:14 +0300

    add task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     len_a, len_b = len(tuple_a), len(tuple_b)
-    
+
     if tuple_a is None or len_a == 0:
         return tuple_b
     elif tuple_b is None or len_b == 0:
@@ -19,14 +19,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new_tuple[1] = tuple_b[1] + tuple_a[1]
 
     return tuple(new_tuple)
-
-
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
-
