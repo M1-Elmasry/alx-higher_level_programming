ef78846c6290d3c42d2758b17ce1b02da06e9f12
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-15 00:54:44 +0300

    update task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = [0, 0]
-    try:
-        new_tuple[0] = tuple_b[0] + tuple_a[0]
-        new_tuple[1] = tuple_b[1] + tuple_a[1]
-        return tuple(new_tuple)
+    len_a, len_b = len(tuple_a), len(tuple_b)
 
-    except IndexError:
-        len_b = len(tuple_b)
-        len_a = len(tuple_a)
-        if len_b > 0:
-            new_tuple[0] += tuple_b[0]
-        if len_a > 0:
-            new_tuple[0] += tuple_a[0]
-        if len_b >= 2:
-            new_tuple[1] += tuple_b[1]
-        if len_a >= 2:
-            new_tuple[1] += tuple_a[1]
-        return tuple(new_tuple)
+    if tuple_a is None or len_a == 0:
+        return tuple_b
+    elif tuple_b is None or len_b == 0:
+        return tuple_a
+
+    new_tuple = [0, 0]
+    new_tuple[0] = tuple_a[0] + tuple_b[0]
+
+    if len_a == 1:
+        new_tuple[1] = tuple_b[1]
+    elif len_b == 1:
+        new_tuple[1] = tuple_a[1]
+    else:
+        new_tuple[1] = tuple_b[1] + tuple_a[1]
+
+    return tuple(new_tuple)
