db5e0d747b722623bd2060ee7b59e4c878c3dc80
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-15 13:33:14 +0300

    update task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a, len_b = len(tuple_a), len(tuple_b)
+    """add first two numbers of two tuple"""
 
-    if tuple_a is None or len_a == 0:
-        return tuple_b
-    elif tuple_b is None or len_b == 0:
-        return tuple_a
+    if tuple_a is None:
+        return tuple(list(tuple_b[:2]))
+    elif tuple_b is None:
+        return tuple(list(tuple_a[:2]))
 
-    new_tuple = [0, 0]
-    new_tuple[0] = tuple_a[0] + tuple_b[0]
+    # add two zeros at the end of the tuple
+    # if the tuple is empty will be the first
+    # else i need first two numbers only
+    tuple_A = list(tuple_a)[:]
+    tuple_A.extend([0, 0])
 
-    if len_a == 1:
-        new_tuple[1] = tuple_b[1]
-    elif len_b == 1:
-        new_tuple[1] = tuple_a[1]
-    else:
-        new_tuple[1] = tuple_b[1] + tuple_a[1]
+    tuple_B = list(tuple_b)[:]
+    tuple_B.extend([0, 0])
+
+    new_tuple = [tuple_A[0] + tuple_B[0], tuple_A[1] + tuple_B[1]]
 
     return tuple(new_tuple)
