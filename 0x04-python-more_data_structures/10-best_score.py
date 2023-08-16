c679ffa7d3da619a040b72e4ee57cca03367eef1
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-17 01:05:56 +0300

    update task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary.keys()) > 0:
         sorted_dict = sorted(a_dictionary.items(), key=lambda a: a[1])
         max_val_key = sorted_dict[-1][0]
         return max_val_key
     else:
         return None
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
