1da51fcca4b5fb73176c9fde9eca1867ef63f74e
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-17 00:56:44 +0300

    add task 1

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        sorted_dict = sorted(a_dictionary.items(), key=lambda a: a[1])
+        max_val_key = sorted_dict[-1][0]
+        return max_val_key
+    else:
+        return None
