43c8734a5f3ca33d59527565bb59ae567c052e48
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-16 22:30:23 +0300

    add task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list is not None:
+        return [i if i != search else replace for i in my_list]
+    else:
+        return None
