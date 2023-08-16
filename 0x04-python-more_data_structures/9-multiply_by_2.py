7fdedf788cff9abba9e66020f9a0a736ac1d76aa
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-17 00:56:44 +0300

    add task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 
 def multiply_by_2(a_dictionary):
     if a_dictionary is not None:
-        return {k:v*2 for k, v in a_dictionary.items()}
+        return {k: v * 2 for k, v in a_dictionary.items()}
     else:
         return None
