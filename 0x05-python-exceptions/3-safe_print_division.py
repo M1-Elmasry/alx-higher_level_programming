08ea2bdf0d9d362790cb994aec5bdfaa1ca57d60
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-29 00:40:05 +0300

    update task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,6 +7,17 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("inside result: ")
+        print("inside result: ", end='')
         print("{}".format(result))
         return result
+
+
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
