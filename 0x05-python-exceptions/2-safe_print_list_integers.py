ec6696648c8bdb22c83004ef256cef2face23fc5
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-29 00:26:29 +0300

    update task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             if type(my_list[i]) is int:
-                print(my_list[i], end="")
+                print("{:d}".format(my_list[i]), end="")
                 counter += 1
 
         except IndexError:
