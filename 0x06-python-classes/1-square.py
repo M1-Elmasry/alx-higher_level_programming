bc085dafa4f9e824fbfcd5fd6a5f0421f8f4e809
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-08-31 05:36:24 +0300

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a simple module represent a one class named Square"""
+
+
+class Square:
+    """a square class"""
+
+    def __init__(self, size):
+        self.__size = size
