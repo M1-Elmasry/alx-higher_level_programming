f03a7500f7558cbb93ee3cb9c360999e93747912
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-02 00:14:48 +0300

    add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""a simple module represent a one class named Square"""
+
+
+class Square:
+    """a square class"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter of size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, new_value):
+        """setter of size property"""
+        if not isinstance(new_value, int):
+            raise TypeError("size must be an integer")
+
+        if new_value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = new_value
+
+    def area(self):
+        """calculate the area of the square"""
+        return self.__size**2
+
+    def my_print(self):
+        """print a square with '#' according to size of the square"""
+        if self.__size == 0:
+            print()
+
+        for _ in range(self.__size):
+            print("#"*self.__size)
