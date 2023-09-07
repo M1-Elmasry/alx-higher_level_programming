da6a314398ce49d8ca07267084e9f44ef2d10d4d
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-07 20:53:01 +0300

    update task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,12 +10,12 @@ def add_integer(a, b=98):
         b (int): second number with default value 98
 
     Raises:
-        TypeError: if a or b is not int
+        TypeError: if a or b is not int and not float
     """
 
-    if not isinstance(a, int):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
-    return a + b
+    return int(a) + int(b)
