a8e3a342a0c1f21c91f92f68ba7efaff3752de7b
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-12 01:51:48 +0300

    add task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,10 @@ and add some features
 class MyList(list):
     """a child class from list"""
 
+    def __init__(self):
+        """init the object"""
+        super().__init__()
+
     def print_sorted(self):
         """print list in ascending order"""
         return print(sorted(self))
