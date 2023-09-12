bac128307c4dab8accb39f4d6c1eac80cc0fc393
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-09-12 05:52:06 +0300

    add task 1

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a module represent a square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a square class that inherite from Rectangle class"""
+
+    def __init__(self, size):
+        """initialization method"""
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
