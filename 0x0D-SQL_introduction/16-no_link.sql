ef2217b7d8af05c525a6d2aa898bc92ac98f327f
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-10-20 03:10:28 +0300

    update task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table with name value
-SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name is NOT NULL ORDER BY score DESC;
