2e3a9446e9054120d6ccdab8b1611b482f606f2c
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-11-14 01:06:54 +0200

    update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,17 @@
 
 // import process module and get argv
 const argv = process.argv;
-// get all numbers
-const numbers = argv.slice(2);
-// convert all string number to numbers and sort the array
-const sortedNumbers = numbers.map((num) => parseInt(num, 10)).sort((a, b) => b - a);
-// print second biggest number
-console.log(sortedNumbers[1]);
+const argvLength = argv.length;
+
+if (argvLength === 2) {
+  console.log(0);
+} else if (argvLength === 3) {
+  console.log(0);
+} else if (argvLength > 3) {
+  // get all numbers
+  const numbers = argv.slice(2);
+  // convert all string number to numbers and sort the array
+  const sortedNumbers = numbers.map((num) => parseInt(num, 10)).sort((a, b) => b - a);
+  // print second biggest number
+  console.log(sortedNumbers[1]);
+}
