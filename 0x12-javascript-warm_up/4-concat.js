1de6857603016ffa6a9f0fcf8d4805c61277ec31
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-11-14 00:56:38 +0200

    Add 4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// import process module and get argv
+const argv = process.argv;
+const firstArg = argv[2];
+const secondArg = argv[3];
+
+console.log(`${firstArg} is ${secondArg}`);
