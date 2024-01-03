3bb9809b76674e0559e90620b6db5a2fa6f9f5c2
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-01-04 01:54:29 +0200

    searching for id instead of full url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,14 @@
 const request = require('request');
 
 const baseUrl = process.argv[2];
-const characterId = 'https://swapi-api.alx-tools.com/api/people/18/';
+const characterId = '18';
 
 request.get(baseUrl, undefined, (err, _res, body) => {
   if (err) console.log(err);
   else {
     const data = JSON.parse(body).results;
     const moviesIdPresents = data.filter((movie) => {
-      return movie.characters.includes(characterId);
+      return movie.characters.some((item) => item.endsWith(`${characterId}/`));
     });
     const idPresentsCount = moviesIdPresents.length;
     console.log(idPresentsCount);
